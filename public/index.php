4cab1dd03823a2c0274b2aef839f71ff64b97e0d
josedomingos919 <josedomingos919@gmail.com>
2021-06-09 21:42:43 +0100

    configuração do home

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teste de Recrutamento @cansei_vendi</title>
+    <link rel="stylesheet" href="./style.css">
+    <script src="./script.js"></script>
+</head>
+<body>
+    <main>
+
+    <section>
+    <header>NOVIDADES</header>
+    </section>
+    
+    </main>
+</body>
+</html>
