bc307fe1ec52d43e6e05ae28eab830e93a88043b
josedomingos919 <josedomingos919@gmail.com>
2021-06-09 23:19:55 +0100

    configuração da api

--- public/index.php
+++ public/index.php
@@ -5,17 +5,40 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Teste de Recrutamento @cansei_vendi</title>
-    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="./style.css"> 
     <script src="./script.js"></script>
 </head>
 <body>
-    <main>
-        <section>
+ <main>
+        <section> 
             <header>NOVIDADES</header>
             <article>
-            
+            <div class="item">
+                <div class="container-img-item">
+                    <img src="" alt="prod" class="img-item" />
+                </div>
+                <div class="text-info">
+                    <p>osiygtdfu podishujdfksdf<br>ds98u67dgyfdfkouy</p>
+                </div>
+                <div class="container-btn-info">
+                    <button class="btn-info">fdsfsf@fdfdsfsdf</button>
+                </div>
+                <div class="info-2">
+                    <h2 class="h-preco">R$ 12.000,00 á vista</h2>
+                    <h3>56gsh78jk9dsld<br>(dsdghijkodl)</h3>
+                </div>
+                <div class="container-ver">
+                    <button class="btn-ver">Ver Produto</button>
+                </div>
+                <div>
+                    <button class="btn-add">
+                        Adicionar na Sacola
+                    </button>
+                </div>
+            </div>
             </article>
         </section>
     </main>
+    
 </body>
 </html>
--- public/script.js
+++ public/script.js
@@ -1,1 +1,7 @@
-document.addEventListener("DOMContentLoaded", () => {});
+window.onload = () => {
+  fetch(
+    "https://dptafza4tn3d0.cloudfront.net/sqatiql8m35w9nm15hwdw0a.json"
+  ).then((e) => {
+    console.log(e);
+  });
+};
